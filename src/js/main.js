956f12477dcc81af7c52201c3a4a248730141aeb
Paco <pacoortiz16@hotmail.com>
2022-10-31 21:20:50 -0600

    main product section finished

---
+++ src/js/main.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () => {
+    // ================= Menu ==============
+    const menuBtn = document.querySelector('.menu-btn');
+    const menu = document.querySelector('.menu');
+    const closeBtn = document.querySelector('.close');
+
+    const menuItem = document.querySelectorAll('.menu-item');
+
+    menuBtn.addEventListener('click', () => {
+        openCloseMenu()
+    })
+
+    closeBtn.addEventListener('click', () => {
+        openCloseMenu()
+    })
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            openCloseMenu()
+        })
+    })
+
+    function openCloseMenu() {
+        menu.classList.toggle('show-menu')
+        menuBtn.classList.toggle('hide');
+        closeBtn.classList.toggle('hide')
+    }
+})
